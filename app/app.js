a9713ae9dfddfe3e182467c708bd2233bc16a4d5
Ramya Praneetha <ramya.praneetha04@gmail.com>
2017-07-07 17:48:21 +0530

    Modules

---
+++ app/app.js
@@ -0,0 +1,13 @@
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+myNinjaApp.config(function(){
+  //fires before the application runs
+});
+
+myNinjaApp.run(function(){
+  //fires while the application runs
+});
+
+myNinjaApp.controller(function(){
+  //fires while the application runs
+});
